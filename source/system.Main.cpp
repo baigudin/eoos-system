b77cadec84585c338652ed21dc83f4d80ba2bb89
Sergey Baigudin <baigudin@mail.ru>
2017-12-13 22:03:34 +0300

    Rule 0-1-3 validated

--- source/system.Main.cpp
+++ source/system.Main.cpp
@@ -22,53 +22,59 @@ namespace system
      */
     int32 Main::main(::api::Kernel& kernel)
     {
-        if( not kernel.isConstructed() ) return -1;    
-        Resource system(kernel);        
-        system_ = &system;
-        kernel_ = &kernel;        
-        int32 stage = 0;
-        int32 error = -1;
-        ::api::Thread* thread = NULL;
-        ::api::Toggle* global = &kernel.getGlobalInterrupt();                
-        ::api::Heap& heap = kernel.getHeap();
-        ::api::Scheduler& scheduler = kernel.getScheduler();        
-        ::system::Allocator::setHeap(heap);        
-        do
+        if( not kernel.isConstructed() ) 
         {
-            // Stage 1: set the system resource factory
-            stage++;
-            if(system_ == NULL || not system_->isConstructed()) break; 
-            // Stage 2: set heap interrupt controller
-            stage++;        
-            heap.setToggle(global);            
-            // Stage 3: create a first user thread
-            stage++;
-            TaskMain task( kernel.getStackSize() );
-            thread = scheduler.createThread(task);
-            if(thread == NULL || not thread->isConstructed() ) break; 
-            // Stage complete: start the first user thread
-            stage = -1;            
-            thread->start();
-            scheduler.yield();
-            thread->join();
-            error = task.error();
+            return -1;
         }
-        while(false);
-        switch(stage)
+        else
         {
-            default:
-            case 3: 
-                delete thread;
-                            
-            case 2: 
-                heap.resetToggle();            
-                            
-            case 1:
+            Resource system(kernel);        
+            system_ = &system;
+            kernel_ = &kernel;        
+            int32 stage = 0;
+            int32 error = -1;
+            ::api::Thread* thread = NULL;
+            ::api::Toggle* global = &kernel.getGlobalInterrupt();                
+            ::api::Heap& heap = kernel.getHeap();
+            ::api::Scheduler& scheduler = kernel.getScheduler();        
+            ::system::Allocator::setHeap(heap);        
+            do
+            {
+                // Stage 1: set the system resource factory
+                stage++;
+                if( not system.isConstructed()) break; 
+                // Stage 2: set heap interrupt controller
+                stage++;        
+                heap.setToggle(global);            
+                // Stage 3: create a first user thread
+                stage++;
+                TaskMain task( kernel.getStackSize() );
+                thread = scheduler.createThread(task);
+                if(thread == NULL || not thread->isConstructed() ) break; 
+                // Stage complete: start the first user thread
+                stage = -1;            
+                thread->start();
+                scheduler.yield();
+                thread->join();
+                error = task.error();
+            }
+            while(false);
+            switch(stage)
+            {
+                default:
+                case 3: 
+                    delete thread;
+
+                case 2: 
+                    heap.resetToggle();            
+                                
+                case 1:
                 
-            case 0: 
-                break;
+                case 0: 
+                    break;
+            }
+            return error;
         }
-        return error;
     }
     
     /**
